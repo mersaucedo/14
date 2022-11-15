e0e4e90fe60d82e5a8fe10fb0ce9a18765972547
mersaucedo <mersaucedosantos@gmail.com>
2022-11-15 16:20:13 -0500

    ejercicios sesion 14

--- script03.js
+++ script03.js
@@ -2,6 +2,6 @@ let entradaGeneral= +prompt("Ingrese la cantidad de entradas generales vendidas"
 let entradaMayor= +prompt("Ingrese la cantidad de entradas para mayores de 65 vendidas");
 let entradaMenor= +prompt("Ingrese la cantidad de entradas para menores vendidas");
 alert(`Se vendio  ${entradaGeneral} entradas general, recaudando ${entradaGeneral*150} soles`);
-alert(`Se vendio  ${entradaMayor} entradas general, recaudando ${entradamayor*65} soles`);
-alert(`Se vendio  ${entradaMenor} entradas general, recaudando ${entradaMenor*80} soles`);
+alert(`Se vendio  ${entradaMayor} entradas mayor, recaudando ${entradaMayor*65} soles`);
+alert(`Se vendio  ${entradaMenor} entradas menor, recaudando ${entradaMenor*80} soles`);
 alert(`El total recaudado es de ${(entradaGeneral*150)+(entradaMayor*65)+(entradaMenor*80)}soles `);
