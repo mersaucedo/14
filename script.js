2a0314d5f4ff8b68e7a0afaa0d30a1b93be44fec
mersaucedo <mersaucedosantos@gmail.com>
2022-11-15 16:28:05 -0500

    ej

--- script.js
+++ script.js
@@ -2,5 +2,5 @@
 let num1= +prompt("Ingrese la cantidad de mujeres");
 let num2= +prompt("Ingrese la cantidad de hombres");
 const total = num1+num2;
-alert(`el total de  alumnos es: ${num1+num2}`);
+alert(`El total de  alumnos es: ${num1+num2}`);
 alert(`El porcentaje de mujeres es: ${(num1/total)*100}% y el porcentaje de hombres es: ${(num2/total)*100}%`);
